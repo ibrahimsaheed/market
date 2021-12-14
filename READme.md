400645924693593864c1a4aa551730e7211b73be
Saheed Ibrahim <ibrahim.saheed2005@gmail.com>
2021-12-14 08:56:53 +0100

    finished adding colour changing program

---
+++ READme.md
@@ -0,0 +1,18 @@
+# Market-place App
+Technologies used
+* css
+* Bootstrap
+* Javascript
+* JQUERY
+* HTML
+## Known Bugs
+* I encounterd problems in assigning values to the letter
+* I encountered problems designing the page
+* Transfering colour from one page to another
+
+# Description
+#### This is a market-place app that allows you to automatically get an appointment
+
+# Author
+#### Saheed Ibrahim
+
--- js/script.js
+++ js/script.js
@@ -1,40 +1,116 @@
 $(document).ready(function () {
-  function getQueryString() {
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-        vars[key] = value;
-    });
-    return vars;
-  }
-  var first = getQueryString()["first"];
-$(".first").append(first);
 
-  $(".form").submit(function (event) {
+
+
+
+
+
+
+  $("#Submit1").click(function (event) {
+    $("#sub1 ").toggle()
     event.preventDefault()
-    $("#Submit").click(function () {
-      $("#sell").toggle()
+    const first = $("#first").val();
+    const second = $("#second").val();
+    const last = $("#last").val();
+    const pass = $("#pass").val();
+    const age = $("#age").val();
+    const number = $("#number").val();
+    const description = $("#description").val();
 
-      let first = $("#first").val();
-      let second = $("#second").val();
-      let last = $("#last").val();
-      let pass = $("#pass").val();
-      let age = $("#age").val();
-      let number = $("#number").val();
-      let description = $("#description").val();
+    $(".first").text(first)
+    $(".second").text(second)
+    $(".last").text(last)
+    $(".pass").text(pass)
+    $(".age").text(age)
+    $(".number").text(number)
+    $(".description").text(description)
+  });
+  $("#Submit2").click(function (event) {
+    $("#sub2 ").toggle()
+    event.preventDefault()
+    const first = $("#first2").val();
+    const second = $("#second2").val();
+    const last = $("#last2").val();
+    const pass = $("#pass2").val();
+    const age = $("#age2").val();
+    const number = $("#number2").val();
+    const description = $("#description2").val();
 
-      $(".first").text(first)
-      $(".second").text(second)
-      $(".last").text(last)
-      $(".pass").text(pass)
-      $(".age").text(age)
-      $(".number").text(number)
-      $(".description").text(description)
+    $(".first").text(first)
+    $(".second").text(second)
+    $(".last").text(last)
+    $(".pass").text(pass)
+    $(".age").text(age)
+    $(".number").text(number)
+    $(".description").text(description)
+  });
+  $("#Submit3").click(function (event) {
+    $("#sub2 ").toggle()
+    event.preventDefault()
+    const first = $("#first1").val();
+    const second = $("#second1").val();
+    const last = $("#last1").val();
+    const pass = $("#pass1").val();
+    const age = $("#age1").val();
+    const number = $("#number1").val();
+    const description = $("#description1").val();
 
-    });
-   
-     
- 
-  })
+    $(".first").text(first)
+    $(".second").text(second)
+    $(".last").text(last)
+    $(".pass").text(pass)
+    $(".age").text(age)
+    $(".number").text(number)
+    $(".description").text(description)
+  });
+  $("button#green").click(function (event) {
+    event.preventDefault()
+    $("body").removeClass();
+    $("body").addClass("green-background ");
 
-})
+  });
+
+  $("button#yellow").click(function (event) {
+    event.preventDefault()
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function (event) {
+    event.preventDefault()
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+  $("button#pink").click(function (event) {
+    event.preventDefault()
+    $("body").removeClass();
+    $("body").addClass("pink-background");
+  });
+  $("button#sunnyred").click(function (event) {
+    event.preventDefault()
+    $("body").removeClass();
+    $("body").addClass("sunnyred-background");
+  });
+  $("button#brown").click(function (event) {
+    event.preventDefault()
+    $("body").removeClass();
+    $("body").addClass("brown-background");
+  });
+  $("button#black").click(function (event) {
+    event.preventDefault()
+    $("body").removeClass();
+    $("body").addClass("black-background");
+  });
+  $("#color").click(function (event) {
+    $(".change ").toggle()
+  });  
+}); 
+
+
+
+
+
+
+
+
 
